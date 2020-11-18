454762dc84b161602e5f2e3838ea420dbcc12d4a
Hagai M <hagaim@mail.tau.ac.il>
2020-11-18 09:52:57 +0200

    Improved error handling on client and server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -5,7 +5,7 @@ use std::process;
 
 use chat_rs::MSG_LENGTH;
 
-fn main() {
+fn main() -> io::Result<()> {
     let address = env::args()
         .nth(1)
         .unwrap_or_else(|| {
@@ -16,18 +16,18 @@ fn main() {
     println!("Connecting to {}:7878", address);
 
     
-    let nick = prompt_msg("Enter nickname: ");
+    let nick = prompt_msg("Enter nickname: ")?;
 
     let mut stream = TcpStream::connect(format!("{}:7878", address))
         .unwrap_or_else(|err| {
             eprintln!("Error on connecting: {}", err.to_string());
             process::exit(1);
         });
-    send_data(1, &nick, &mut stream);
+    send_data(1, &nick, &mut stream)?;
     println!("Connected.");
     
     loop {
-        let mut string = prompt();
+        let mut string = prompt()?;
         string.truncate(MSG_LENGTH - 1);
 
         if string == "exit" {
@@ -35,28 +35,30 @@ fn main() {
         }
         
         let code = 0;
-        send_data(code, &string, &mut stream);
-    }
+        send_data(code, &string, &mut stream)?;
+    };
+    io::Result::Ok(())
 }
 
 
-fn send_data(code: u8, string: &str, stream: &mut TcpStream) {
+fn send_data(code: u8, string: &str, stream: &mut TcpStream) -> io::Result<()> {
     let mut buffer = Vec::with_capacity(MSG_LENGTH);
     buffer.push(code);
     buffer.extend(string.as_bytes());
 
-    stream.write_all(&buffer).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(&buffer)?;
+    stream.flush()?;
+    io::Result::Ok(())
 }
 
-fn prompt() -> String {
+fn prompt() -> io::Result<String> {
     let mut string = String::with_capacity(MSG_LENGTH + 1);
-    io::stdin().read_line(&mut string).unwrap();
-    string.trim().to_string()
+    io::stdin().read_line(&mut string)?;
+    Ok(string.trim().to_string())
 }
 
-fn prompt_msg(string: &str) -> String {
+fn prompt_msg(string: &str) -> io::Result<String> {
     print!("{}", string);
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
     prompt()
 }
