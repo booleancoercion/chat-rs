2556d3f22a2b9ded9964698fef0a5dd7067d5de9
booleancoercion <booleancoercion@gmail.com>
2020-11-19 14:29:33 +0200

    Moved thread spawning outside of handle_connection

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -30,7 +30,10 @@ fn main() -> io::Result<()>{
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_connection(stream, users.clone());
+                let uclone = users.clone();
+                thread::spawn(move || {
+                    handle_connection(stream, uclone);
+                });
             },
             Err(_) => continue
         }
@@ -40,52 +43,49 @@ fn main() -> io::Result<()>{
 
 fn handle_connection(stream: TcpStream, users: Arc<Mutex<HashSet<User>>>) {
     let mut stream = ChatStream{stream};
+    let peer_address = stream.peer_addr().unwrap();
 
-    thread::spawn(move || {
-        let peer_address = stream.peer_addr().unwrap();
+    let mut buffer = [0u8; MSG_LENGTH];
 
-        let mut buffer = [0u8; MSG_LENGTH];
+    let nick = match stream.receive_data(&mut buffer) {
+        Ok(Msg::NickChange(nick)) => nick,
+        _ => {
+            println!("{} aborted on nick.", peer_address);
+            return
+        }
+    };
 
-        let nick = match stream.receive_data(&mut buffer) {
-            Ok(Msg::NickChange(nick)) => nick,
-            _ => {
-                println!("{} aborted on nick.", peer_address);
-                return
+    if users.lock().unwrap().len() >= MAX_USERS {
+        stream.send_data(Msg::TooManyUsers)
+            .unwrap_or_else(|_| {}); // do nothing, we don't need the user anyway
+        println!("Rejected {}, too many users", peer_address);
+        return
+    } else {
+        stream.send_data(Msg::ConnectionAccepted).unwrap();
+        println!("Connection from {}, nick {}", peer_address, nick);
+    }
+
+    let temp_user = User {
+        nick: nick.clone(),
+        stream: stream.try_clone()
+            .expect(&format!("Couldn't clone stream for {}", peer_address))
+    };
+    users.lock().unwrap().insert(temp_user);
+
+
+    loop {
+        let msg = match stream.receive_data(&mut buffer) {
+            Ok(msg) => msg,
+            Err(e) => {
+                println!("{} [{}] disconnected with error {}.", peer_address, nick, e.to_string());
+                users.lock().unwrap().remove(&User {
+                    nick,
+                    stream
+                });
+                break
             }
         };
 
-        if users.lock().unwrap().len() >= MAX_USERS {
-            stream.send_data(Msg::TooManyUsers)
-                .unwrap_or_else(|_| {}); // do nothing, we don't need the user anyway
-            println!("Rejected {}, too many users", peer_address);
-            return
-        } else {
-            stream.send_data(Msg::ConnectionAccepted).unwrap();
-            println!("Connection from {}, nick {}", peer_address, nick);
-        }
-
-        let temp_user = User {
-            nick: nick.clone(),
-            stream: stream.try_clone()
-                .expect(&format!("Couldn't clone stream for {}", peer_address))
-        };
-        users.lock().unwrap().insert(temp_user);
-
-
-        loop {
-            let msg = match stream.receive_data(&mut buffer) {
-                Ok(msg) => msg,
-                Err(e) => {
-                    println!("{} [{}] disconnected with error {}.", peer_address, nick, e.to_string());
-                    users.lock().unwrap().remove(&User {
-                        nick,
-                        stream
-                    });
-                    break
-                }
-            };
-
-            println!("Msg({}): [{}]: {}", msg.code(), nick, msg.string());
-        }
-    });
+        println!("Msg({}): [{}]: {}", msg.code(), nick, msg.string());
+    }
 }
