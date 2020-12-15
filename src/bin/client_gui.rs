44e2e09f353c62785b9c7999b51d37d7214a47bb
booleancoercion <booleancoercion@gmail.com>
2020-12-15 23:22:07 +0200

    Made lame progress on client_gui

--- src/bin/client_gui.rs
+++ src/bin/client_gui.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Sorry, not yet implemented.");
+use iced::{Element, Sandbox, Settings, Text};
+
+use chat_rs::{ChatStream, Msg};
+
+pub fn main() -> iced::Result {
+    ChatClient::run(Settings::default())
+}
+
+struct ChatClient {
+    messages: Vec<Msg>
+}
+
+impl Sandbox for ChatClient {
+    type Message = ();
+
+    fn new() -> ChatClient {
+        ChatClient {
+            messages: vec![]
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Resizing this is laggy AF")
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // This application has no interactions
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }
